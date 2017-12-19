2c446d5868a3695320b2f57a27cf9b32745cac50
Lance <603842325@qq.com>
2017-12-19 18:32:14 +0800

    hey!

--- 哈尔滨理工大学/“科林明伦杯”哈尔滨理工大学第七届程序设计团队赛/正式/A.cpp
+++ 哈尔滨理工大学/“科林明伦杯”哈尔滨理工大学第七届程序设计团队赛/正式/A.cpp
@@ -1,22 +1,21 @@
-#include <iostream>
+#include <cstdio>
 #include <cmath>
 #include <algorithm>
-#include <cstring>
 using namespace std;
-const double er = sqrt(2);
-int a[50005];
+const double er = sqrt(2);                                 //¸ùºÅ¶þ 
+const int MAXN = 50005;
+int a[MAXN];
 int main() {
-	int t, cnt = 1;
-	cin >> t;
-	while (t--) {
-		int n, q;
-		cin >> n >> q;
+	int T, cnt = 1, n, q;
+	scanf("%d", &T);
+	while (T--) {
+		scanf("%d %d", &n, &q);
 		printf("Case #%d:\n", cnt++);
 		while (q--) {
-			for (int i = 0; i < n; i++) cin >> a[i];
+			for (int i = 0; i < n; ++i) scanf("%d", &a[i]);
 			sort(a, a + n);
-			int ans = INT_MAX;
-			for (int i = 1; i < n; i++) ans = min(a[i] - a[i - 1], ans);
+			int ans = a[0];                                 
+			for (int i = 1; i < n; ++i) ans = min(a[i] - a[i - 1], ans);
 			printf("%.4lf\n", ans * 1.0 / er);
 		}
 	}
